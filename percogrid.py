50cd75d32e77a0e353a4e74e6aa6ebc3ad05e83c
ReblochonMasque <trustgold@gmail.com>
2021-02-04 21:03:38 +0800

    massive revamp with two UF data structures

--- percogrid.py
+++ percogrid.py
@@ -10,30 +10,7 @@ to the top row and that process fills some OPENED site on the bottom row
 """
 
 from congeries import WeightedQuickUnionPathCompressionUF
-from enum import Enum
-
-
-class Site(Enum):
-    """a Site is either OPENED or BLOCKED
-    """
-    BLOCKED = 0
-    OPENED = 1
-
-    def is_blocked(self):
-        return self is self.BLOCKED
-
-    def is_open(self):
-        """check if is OPEN or is FULL
-
-        :return: True is self is OPEN or self is FULL, False otherwise
-        """
-        return self is not self.BLOCKED
-
-    def __str__(self):
-        if self is self.BLOCKED:
-            return '\u2588'
-        else:
-            return ' '
+from typing import List
 
 
 class PercoGrid:
@@ -56,110 +33,75 @@ class PercoGrid:
             raise ValueError('n must be > 0')
         self.rows = self.cols = n
         self.size = self.rows * self.cols
-        self.grid = [[Site.BLOCKED for col in range(n)] for row in range(n)]
-        self.number_open_sites = 0
-        self.uf = WeightedQuickUnionPathCompressionUF(self.size+2)
-        self.top_ndx, self.bot_ndx = self.size, self.size + 1
-        self._connect_top_and_bottom()
+        self.opened_site = set()
+        self.uf_top = WeightedQuickUnionPathCompressionUF(self.size+2)
+        self.uf_bot = WeightedQuickUnionPathCompressionUF(self.size+2)
+        self.virtual_top, self.virtual_bot = 0, self.size + 1
+        self._percolates = False
 
-    def _connect_top_and_bottom(self):
-        for idx in range(self.cols):
-            self.uf.union(self.top_ndx, idx)
-        for idx in range(self.rows * (self.cols - 1), self.size):
-            self.uf.union(self.bot_ndx, idx)
+    def _is_valid_row_col(self, row: int, col: int) -> bool:
+        return 1 <= row <= self.rows and 1 <= col <= self.cols
 
-    def _base_1_to_base_0(self, row, col):
-        if not self._is_valid_row_base_1(row):
-            raise ValueError('row must be >= 1 and <= {self.rows}')
-        if not self._is_valid_col_base_1(col):
-            raise ValueError('col must be >= 1 and <= {self.cols}')
-        return row - 1, col - 1
+    def _get_flat_index(self, row: int, col: int) -> int:
+        return (row - 1) * self.cols + col - 1
 
-    def _is_valid_row_base_1(self, row: int) -> bool:
-        return 1 <= row <= self.rows
-
-    def _is_valid_col_base_1(self, col: int) -> bool:
-        return 1 <= col <= self.cols
+    def _get_flat_neighbors(self, row: int, col: int) -> List[int]:
+        neighbors = []
+        for dr, dc in self.neighbor_offsets:
+            nr, nc = row + dr, col + dc
+            if self._is_valid_row_col(nr, nc):
+                neighbors.append(self._get_flat_index(nr, nc))
+        return neighbors
 
     def open(self, row: int, col: int) -> None:
-        """opens the site (row, col) if it is not OPENED already
-
-        :param row: int, the row
-        :param col: col, the col
-        :return: None
-        """
-        r, c = self._base_1_to_base_0(row, col)
-        if self.grid[r][c].is_blocked():
-            self.grid[r][c] = Site.OPENED
-            self.number_open_sites += 1
-            self._connect_with_open_neighbors(r, c)
-
-    def _connect_with_open_neighbors(self, r: int, c: int):
-        n = self._get_flat_index(r, c)
-        for dr, dc in self.neighbor_offsets:
-            rdr, cdc = r + dr, c + dc
-            if self._is_valid_r_base0(rdr) and self._is_valid_c_base_0(cdc):
-                if self._isopen_base_0(rdr, cdc):
-                    self.uf.union(n, self._get_flat_index(rdr, cdc))
-
-    def _is_valid_r_base0(self, r: int) -> bool:
-        return 0 <= r < self.rows
-
-    def _is_valid_c_base_0(self, c: int) -> bool:
-        return 0 <= c < self.cols
-
-    def _get_flat_index(self, r: int, c: int) -> int:
-        return r * self.cols + c
+        site = self._get_flat_index(row, col)
+        if site in self.opened_site:
+            return
+        self.opened_site.add(site)
+        for neighbor in self._get_flat_neighbors(row, col):
+            if neighbor in self.opened_site:
+                self.uf_top.union(site, neighbor)
+                self.uf_bot.union(site, neighbor)
+        if row == 1:
+            self.uf_top.union(site, self.virtual_top)
+        if row == self.rows:
+            self.uf_bot.union(site, self.virtual_bot)
+        if self.uf_top.connected(site, self.virtual_top) and \
+                self.uf_bot.connected(site, self.virtual_bot):
+            self._percolates = True
 
     def isopen(self, row: int, col: int) -> bool:
-        """is the site at pos (row, col) OPENED?
-
-        :param row: int, the row
-        :param col: col, the col
-        :return: True if the site is OPENED, False otherwise
-        """
-        return self._isopen_base_0(*self._base_1_to_base_0(row, col))
+        return self._get_flat_index(row, col) in self.opened_site
 
     def isfull(self, row, col):
-        return self._isfull_base_0(*self._base_1_to_base_0(row, col))
-
-    def _isopen_base_0(self, r: int, c: int) -> bool:
-        return self.grid[r][c].is_open()
-
-    def _isfull_base_0(self, r: int, c: int) -> bool:
-        return self._isopen_base_0(r, c) and self.uf.connected(self._get_flat_index(r, c), self.top_ndx)
+        return self.uf_top.connected(self._get_flat_index(row, col), self.virtual_top)
 
     def percolates(self) -> bool:
-        """does the system percolate?
-
-        :return: True if percolates, False otherwise
-        """
-        return self.uf.connected(self.top_ndx, self.bot_ndx)
+        return self._percolates
 
     def __str__(self):
-        result = []
-        for r, row in enumerate(self.grid):
-            res = []
-            for c, site in enumerate(row):
-                s = str(site)
-                if self._isfull_base_0(r, c):
-                    s = '.'
-                res.append(s)
-            result.append(''.join(res))
-        return '\n'.join(result)
+        res = []
+        for row in range(1, n+1):
+            for col in range(1, n+1):
+                c = 'X'
+                if self.isopen(row, col):
+                    c = ' '
+                if self.isfull(row, col):
+                    c = '.'
+                res.append(c)
+            res.append('\n')
+        return ''.join(res)
 
 
 if __name__ == '__main__':
 
-    pg = PercoGrid(6)
-    while True:
-        r = input('row:')
-        c = input('col:')
-        pg.open(int(r), int(c))
+    sites = [(5, 1), (4, 1), (3, 1), (1, 1), (2, 2), (2, 3), (3, 3), (4, 3), (4, 4), (4, 5), (5, 5), (1, 2)]
+    n = 5
+    pg = PercoGrid(n)
+    for row, col in sites:
+        pg.open(row, col)
         print(pg)
-        print(pg.uf.id)
         if pg.percolates():
-            print('PERCOLATES!')
+            print('PERCOLATES!!')
             break
-
-    print('done!')
+    print(pg)
