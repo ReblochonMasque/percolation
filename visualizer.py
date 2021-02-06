312e7b476538ed7cb4205a0cd0dc2dafec31e624
ReblochonMasque <trustgold@gmail.com>
2021-02-06 12:23:33 +0800

    add dashboard

--- visualizer.py
+++ visualizer.py
@@ -66,15 +66,37 @@ class PercolationGridView(tk.Canvas):
 class PercoFrame(tk.Frame):
     _grid_width, _grid_height = 600, 600
 
-    def __init__(self, master, n: int):
+    def __init__(self, master, controller, n: int):
         self.master = master
         super().__init__(self.master)
+        self.controller = controller
         self.n = n
         _gw = _gh = val_closest_to(self.n, PercoFrame._grid_width)
         self.grid_width = _gw + PercolationGridView.left_offset * 2
         self._grid_height = _gh + PercolationGridView.top_offset * 2
         self.percocanvas = PercolationGridView(self, n, width=self.grid_width, height=self._grid_height)
         self.percocanvas.pack()
+
+        self.dashboard = tk.Frame(self)
+
+        self.left_dash = tk.Frame(self)
+        self.open_sites_var = tk.StringVar()
+        self.open_sites_var.set(f'open sites: {len(self.controller.perco.opened_site)}')
+        self.display_open_sites_lbl = tk.Label(self.left_dash, textvariable=self.open_sites_var)
+        self.display_open_sites_lbl.pack()
+
+        self.connected_components_var = tk.StringVar()
+        self.connected_components_var.set(f'connected_components: {self.controller.perco.uf_top.components_count}')
+        self.display_connected_components_lbl = tk.Label(self.left_dash, textvariable=self.connected_components_var)
+        self.display_connected_components_lbl.pack()
+        self.left_dash.pack(side=tk.LEFT)
+
+        self.percolates_var  = tk.StringVar()
+        self.percolates_var.set(f'percolates: {self.controller.perco.percolates()}')
+        self.display_percolates_var_lbl = tk.Label(self.dashboard, textvariable=self.percolates_var)
+        self.display_percolates_var_lbl.pack(side=tk.RIGHT)
+
+        self.dashboard.pack(fill=tk.X)
 
 
 class Controller:
@@ -83,7 +105,7 @@ class Controller:
         self.n = n
         self.perco = Percolation(self.n)
         self.master = tk.Tk()
-        self.percoframe = PercoFrame(self.master, self.n)
+        self.percoframe = PercoFrame(self.master, self, self.n)
         self.percoframe.pack()
 
 
@@ -122,6 +144,6 @@ if __name__ == '__main__':
     #
     # tk.Button(root, text='run', command=lambda: run(pg, pv, sites)).pack()
 
-    c = Controller(10)
+    contr = Controller(10)
 
-    c.master.mainloop()
+    contr.master.mainloop()
