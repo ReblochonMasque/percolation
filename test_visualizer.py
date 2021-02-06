dc1cf82011031538509882b8a78a7cc8440877d3
ReblochonMasque <trustgold@gmail.com>
2021-02-06 11:01:05 +0800

    add TestVisualizer test_val_closest_to_1()

--- test_visualizer.py
+++ test_visualizer.py
@@ -10,6 +10,11 @@ class TestVisualizer(unittest.TestCase):
         actual = val_closest_to(11, 100)
         self.assertEqual(expected, actual)
 
+    def test_val_closest_to_1(self):
+        expected = 611
+        actual = val_closest_to(47, 600)
+        self.assertEqual(expected, actual)
+
 
 if __name__ == '__main__':
     unittest.main()
